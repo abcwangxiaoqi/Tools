c9435610420b00cd99cdb51eab3cd09c30819411
frank <489280591@qq.com>
2018-08-30 22:04:24 +0800

    no message

---
+++ FileOperate/api.py
@@ -0,0 +1,48 @@
+
+import threading
+
+__fileLock = threading.Lock()
+
+def __writefile(file,start,end,content):
+
+    if __fileLock.acquire():
+        f = open(file,'wb')
+        f.seek(start)
+        f.write(content[start:end])
+        f.close()
+        __fileLock.release()
+
+    return
+
+def MultiThreadWrite(file,content,threadNum = 2):
+
+    if threadNum < 2 :
+        threadNum = 2
+
+    fileSize = len(content)
+    everySize = int(fileSize / threadNum)
+
+    threads = []
+    start = [threadNum]
+    end = [threadNum]
+
+    for i in range(threadNum):
+        if i + 1 == threadNum:
+            start[i] = i * everySize
+            end[i] = fileSize - 1
+            continue
+        start.insert(i, i * everySize)
+        end.insert(i, (i + 1) * everySize - 1)
+
+    for n in range(threadNum):
+        t = threading.Thread(target=__writefile,args=[file,start[n],end[n],content])
+        threads.append(t)
+
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    return
--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 ## 记录python开发集成的工具，以便使用  
 
-[Loader](Loader/README.md)
++ ***Loader*** 
+
+**用于Http请求的工具，在requests库上进行二次封装**
+1. 多线程处理下载任务
+2. 多线程下载同一资源  
+
+**使用方式：**  
+
+```
+#导入
+from Loader import *
+
+# 初始化 ，可传参数，参数为开启线程数，默认为3
+myrequests.init(3)
+
+#Get Post 比较与requests多了个回调，其他参数按照requests方式传递即可
+myrequests.Get(url,callback,省略)
+myrequests.Post(url,callback,省略)
+
+#多线程下载资源 threadNum 同时下载启动线程数 Get Post
+myrequests.GetMultiDown(url, callback,threadNum = 2, 省略)
+myrequests.GetMultiDown(url, callback,threadNum = 2, 省略)
+
+#停止
+myrequests.Stop()
+
+```  
+
++ ***FileOperate***  
+
+**文件操作工具**  
+1. 多线程写入文件 
+
+**使用方法:**  
+```
+
+```  
+**注意:** 对于请求网络数据，多线程可能能够提高效率，但是对于本地磁盘 IO 操作，多线程大部分时候反而会降低性能，原因在于机械硬盘写入数据时候磁头需要不断移动定位。
